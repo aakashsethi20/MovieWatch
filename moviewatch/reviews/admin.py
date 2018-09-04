64785909c46d11c9e9fd34de8a4c962509cd49f8
Aakash Sethi <aakashsethi20@gmail.com>
2018-09-04 16:29:02 -0400

    add models to reviews and register them to admin

--- moviewatch/reviews/admin.py
+++ moviewatch/reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie, Review
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('movie', 'rating', 'user_name', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comment']
+
+class MovieAdmin(admin.ModelAdmin):
+    model = Movie
+    list_display = ('name',)
+    search_fields = ['name']
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review, ReviewAdmin)
--- moviewatch/reviews/models.py
+++ moviewatch/reviews/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+import numpy as np
+
+class Movie(models.Model):
+    name = models.CharField(max_length=200)
+
+    def average_rating(self):
+        all_ratings = map(lambda x: x.rating, self.review_set.all())
+        return np.mean(all_ratings)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+    user_name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=500)
+    rating = models.IntegerField(choices=RATING_CHOICES)
