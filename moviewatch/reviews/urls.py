737adba83a4587fd0b7d1fd8a6c798c85c177815
Aakash Sethi <aakashsethi20@gmail.com>
2018-09-06 15:12:16 -0400

    add form for review

--- moviewatch/reviews/urls.py
+++ moviewatch/reviews/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
     path('movie/<int:movie_id>', views.movie_detail, name='movie_detail'),
+    path('movie/<int:movie_id>/add_review', views.add_review, name='add_review'),
     path('reviews', views.review_list, name='review_list'),
     path('reviews/<int:review_id>', views.review_detail, name='review_detail'),
 ]
--- moviewatch/reviews/views.py
+++ moviewatch/reviews/views.py
@@ -1,6 +1,11 @@
+import datetime
+
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Review, Movie
+from .forms import ReviewForm
 
 def movie_list(request):
     movie_list = Movie.objects.order_by('name')
@@ -9,7 +14,8 @@ def movie_list(request):
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
-    context = {'movie': movie}
+    form = ReviewForm()
+    context = {'movie': movie, 'form': form}
     return render(request, 'reviews/movie_detail.html', context)
 
 def review_list(request):
@@ -21,3 +27,25 @@ def review_detail(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
     context = {'review': review}
     return render(request, 'reviews/review_detail.html', context)
+
+def add_review(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        user_name = form.cleaned_data['user_name']
+        rating = form.cleaned_data['rating']
+        comment = form.cleaned_data['comment']
+        review = Review()
+        review.movie = movie
+        review.user_name = user_name
+        review.rating = rating
+        review.comment = comment
+        review.pub_date = datetime.datetime.now()
+        review.save()
+
+        # Should return HttpResponseRedirect with POST data to avoid posting
+        # twice if the user clicks browser's back button
+        return HttpResponseRedirect(reverse('reviews:movie_detail', args=(movie.id,)))
+
+    context = {'movie': movie, 'form': form}
+    return render(request, 'reviews/movie_detail.html', context)
